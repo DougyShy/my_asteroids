6d8447f302667ff91d9fbe03ec4d9fa8f5ff2d7c
sch31 <sch31ble@gmail.com>
2026-01-24 16:57:52 -0600

    beginner code

---
+++ main.py
@@ -0,0 +1,32 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+def main():
+    pygame.init()
+    print(f"Starting Asteroids with pygame version: " + pygame.version.ver)
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Infinite game loop
+    while True:
+        # Log current game state
+        log_state()
+
+        # Start processing the pygame event queue
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                return
+
+
+        screen.fill("black")
+        pygame.display.flip()
+
+
+
+
+if __name__ == "__main__":
+    main()
