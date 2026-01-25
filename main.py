22d1f9b8c65f8d0e33fb6d9fc7996b1500f123eb
sch31 <sch31ble@gmail.com>
2026-01-25 11:53:44 -0600

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_RADIUS
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -9,6 +10,11 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     # Infinite game loop
     while True:
@@ -23,9 +29,11 @@ def main():
 
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
-
+        clock.tick(60)
+        dt = clock.tick() / 1000       
 
 
 if __name__ == "__main__":
